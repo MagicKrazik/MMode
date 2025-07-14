3b74606b7713b0955f5508d055eedf351c15bf0c
Carlos <carlos.horacio.olivares@gmail.com>
2025-07-13 23:01:10 -0600

    updated settings

--- monkmode_productivity/monkmode_productivity/settings.py
+++ monkmode_productivity/monkmode_productivity/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-3)citwx#7awlsky+g!0_h7po@)!hriaf+k((3(wy0y0-d8v@i1'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
